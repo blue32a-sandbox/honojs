6c8bc0951c3a390ee72be281c3ef7614368f9a09
blue32a <blue32a.jp@gmail.com>
2024-04-11 03:37:21 +0000

    Return JSON

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ const app = new Hono()
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+app.get('/api/hello', (c) => {
+  return c.json({
+    ok: true,
+    message: 'Hello Hono!',
+  })
+})
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
