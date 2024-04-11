65298e8c82e3d2ede7174fb46ad4a42c34d25da4
blue32a <blue32a.jp@gmail.com>
2024-04-11 03:43:47 +0000

    Request and Response

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,16 @@ const app = new Hono()
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/posts/:id', (c) => {
+  const page = c.req.query('page')
+  const id = c.req.param('id')
+  c.header('X-Message', 'Hi!')
+  return c.text(`You want see ${page} of ${id}`)
+})
+app.post('/posts', (c) => c.text('Created!', 201))
+app.delete('/posts/:id', (c) => c.text(`${c.req.param('id')} is deleted!`))
+
 app.get('/api/hello', (c) => {
   return c.json({
     ok: true,
